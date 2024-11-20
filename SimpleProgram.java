57f3e502b9c924ee140b7dafc5b17658b953c1dd
mytest4mail03 <mytest4mail03@gmail.com>
2024-11-20 19:44:59 +1100

    Create SimpleProgram.java

---
+++ SimpleProgram.java
@@ -0,0 +1,6 @@
+public class SimpleProgram {
+    public static void main(String[] args) {
+        // Print a message
+        System.out.println("Hello, World! Welcome to Java programming.");
+    }
+}
